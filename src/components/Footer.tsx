c2da7af681842aacf955c7877b5642fda7224f5f
Ananya Lohani <61914211+ananyalohani@users.noreply.github.com>
2021-05-21 17:36:20 +0530

    Test deployment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
         style={{ height: '0.5px', width: '100%' }}
         className='bg-white opacity-20 mx-auto mt-3 sm:hidden'
       ></div>
-      <p className='sm:text-base text-center font-light opacity-50 mb-5 mt-3 sm:mt-0 sm:mb-3'>
+      <p className='sm:text-base text-center font-light opacity-50 mb-5 mt-3 sm:mt-0 sm:mb-3 '>
         Made by{' '}
         <a href='https://ananyalohani.me/' className='font-medium opacity-100'>
           Ananya Lohani
